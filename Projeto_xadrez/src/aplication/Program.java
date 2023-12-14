694c3ad5696727563f66748fea4be7f25c263a82
Juan Nogueira <juannogueiraxavier@gmail.com>
2023-12-14 14:35:06 -0300

    Starting to implement: Board, Piece

--- Projeto_xadrez/src/aplication/Program.java
+++ Projeto_xadrez/src/aplication/Program.java
@@ -1,14 +1,14 @@
 package aplication;
 
-import boardGame.Position;
+import boardGame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position(3, 5);
+		Board board = new Board(8, 8);
 		
-		System.out.println(pos);
+		System.out.println(board);
 
 	}
 
