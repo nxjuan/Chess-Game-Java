c264a56572266622874906f4c05e2e4b62e02f63
Juan Nogueira <juannogueiraxavier@gmail.com>
2023-12-14 14:19:36 -0300

    Class Program and Class

---
+++ Projeto_xadrez/src/aplication/Program.java
@@ -0,0 +1,15 @@
+package aplication;
+
+import boardGame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
+
+	}
+
+}
