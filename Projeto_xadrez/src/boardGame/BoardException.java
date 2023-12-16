5d5cd3709a3c6f7b9925ca3c1088b4937d3bcf5a
Juan Nogueira <juannogueiraxavier@gmail.com>
2023-12-16 20:45:07 -0300

    BoardException and defensive Programming

---
+++ Projeto_xadrez/src/boardGame/BoardException.java
@@ -0,0 +1,11 @@
+package boardGame;
+
+public class BoardException extends RuntimeException{
+
+	public BoardException(String msg) {
+		super(msg);
+	}
+	
+	private static final long serialVersionUID = 1L;
+	
+}
