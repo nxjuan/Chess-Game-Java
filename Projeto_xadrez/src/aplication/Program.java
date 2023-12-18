e242f748d435dd070ea9fd09f936ddd9a50fc902
Juan Nogueira <juannogueiraxavier@gmail.com>
2023-12-18 14:58:41 -0300

    Moving pieces

--- Projeto_xadrez/src/aplication/Program.java
+++ Projeto_xadrez/src/aplication/Program.java
@@ -1,16 +1,32 @@
 package aplication;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
-import boardGame.Piece;
-import boardGame.Position;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {	
+		
+		Scanner sc = new Scanner(System.in);
 		
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
 		
+		
+		while(true) {
+			UI.printBoard(chessMatch.getPieces());
 			
+			System.out.println();
+			
+			System.out.print("Sorce:  ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.print("Target:  ");
+			ChessPosition target = UI.readChessPosition(sc); 
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}				
 	}
 }
